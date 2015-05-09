41aab78ef702a32ca853672b6c47a31482655112
Andrzej Podgorski <andrzej.podgorski@hotmail.com>
2015-05-09 17:14:17 +0200

    added collections input from stdin and memory allocation + copying host to device

--- cuda_functions.cu
+++ cuda_functions.cu
@@ -1,9 +1,26 @@
-__global__ void classify(int * teached)
-{
 
+__global__ void countDistances(int dimensions, float *teachingCollection, int teachingCollectionCount, float *classifyCollection, int classifyCollectionCount, float *distances)
+{
+	//compute :P
 }
 
-void cuda_knn()
+void cuda_knn(int dimensions, float *h_teachingCollection, int *h_teachedClasses, int teachingCollectionCount, float *h_classifyCollection, int *h_classifiedClasses, int classifyCollectionCount)
 {
-	classify<<<100,100>>>(NULL);
+	int ierr;
+	float *d_teachingCollection, *d_classifyCollection;
+	ierr = cudaMalloc(&d_teachingCollection, teachingCollectionCount*dimensions*sizeof(float));
+	ierr = cudaMalloc(&d_classifyCollection, classifyCollectionCount*dimensions*sizeof(float));
+
+	ierr = cudaMemcpy(d_teachingCollection, h_teachingCollection, teachingCollectionCount*dimensions, cudaMemcpyDeviceToHost);
+	ierr = cudaMemcpy(d_classifyCollection, h_classifyCollection, classifyCollectionCount*dimensions, cudaMemcpyDeviceToHost);
+
+	float *d_distances;
+	cudaMalloc(&d_distances, teachingCollectionCount*classifyCollectionCount*sizeof(float));
+	
+	const int threadsPerBlock = 100;
+	int blocksPerGrid = classifyCollectionCount / 100 + 1;
+		
+	countDistances<<<blocksPerGrid, threadsPerBlock>>>(dimensions, d_teachingCollection, teachingCollectionCount, d_classifyCollection, classifyCollectionCount, d_distances);
+	cudaFree(d_teachingCollection);
+	cudaFree(d_classifyCollection);
 }
--- main.cpp
+++ main.cpp
@@ -8,9 +8,48 @@
 int main(int argc, char ** argv)
 {
 	MPI_Init(&argc, &argv);
-	cuda_knn();
-	std::cout << "ok"; 
+	int myId, ierr;
+	ierr = MPI_Comm_rank(MPI_COMM_WORLD, &myId);
+	if (myId == 0)
+	{
+		int dimensions;
+		std::cin >> dimensions;
+		
+		int teachingCollectionCount;
+		std::cin >> teachingCollectionCount;
+		float *teachingCollection = new float[teachingCollectionCount*dimensions];
+		int *teachedClasses = new int[teachingCollectionCount];
+		for (int i=0; i< teachingCollectionCount; i++)
+		{
+			for (int j=0; j<dimensions; j++)
+			{
+				std::cin >> teachingCollection[i*dimensions+j];
+			}
+			std::cin >> teachedClasses[i];
+		}
+		// to this moment - definition of teaching collection
+		
+		int classifyCollectionCount;
+		std::cin >> classifyCollectionCount;
+		float *classifyCollection = new float[classifyCollectionCount*dimensions];
+		int *classifiedClasses = new int[classifyCollectionCount];
+		for (int i=0; i<classifyCollectionCount; i++)
+		{
+			for (int j=0; j<dimensions; j++)
+			{
+				std::cin >> classifyCollection[i*dimensions+j];
+			}
+		}
+		// to this moment - deffinition of collection to classify
+	
+	
+	cuda_knn(dimensions, teachingCollection, teachedClasses, teachingCollectionCount, classifyCollection, classifiedClasses, classifyCollectionCount);
+	
+	std::cout << "ok\n"; 
 
+	delete[] teachingCollection;
+	delete[] teachedClasses;
+	}
 	MPI_Finalize();
 
 }
